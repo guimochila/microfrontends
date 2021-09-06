b04a63e219b38a11efdce850508059512b39e6bf
Guilherme <gui.mochila@gmail.com>
2021-09-06 11:38:57 +0200

    Add routing feature for microfrontends

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 function MarketingApp() {
   const ref = React.useRef(null);
+  const history = useHistory();
 
   React.useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate({ pathname: nextPathname }) {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
 
     return () => (ref.current = null);
   }, []);
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
 
 import Landing from './components/Landing';
@@ -9,16 +9,16 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'ma',
 });
 
-function App() {
+function App({ history }) {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route exact path="/" component={Landing} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
     </div>
   );
